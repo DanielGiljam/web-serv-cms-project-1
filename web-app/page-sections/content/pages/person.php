d091b9bdc63f5f02e4738d3c30d88842780ca877
Mattias Kallman <33825140+kallmanm@users.noreply.github.com>
2018-05-08 20:39:06 +0300

    added googlemaps api

--- web-app/page-sections/content/pages/person.php
+++ web-app/page-sections/content/pages/person.php
@@ -5,12 +5,11 @@
 # Procedurally generates a person page, determining how to generate it
 # based on the app state information provided by the AppStateCentral -object.
 
-//todo: make session handling
+//todo: make session handling and redirect if not logged in...
 
 if (isset($page_specific_properties['no_such_user']) && $page_specific_properties['no_such_user']) {
 
 ?>
-
 <p>Your requested user does not exist on this site.</p>
 
 <?php
@@ -31,6 +30,53 @@ if (isset($page_specific_properties['no_such_user']) && $page_specific_propertie
     </ul>
 </p>
 
+    <div id="zip_code_geocoding"style="display:none"><?php echo $page_specific_properties['person']->get('zip_code')->value() ?></div>
+    <div id="map" style="width: 360px; height: 360px;" onload="initMap()"></div>
+    <script>
+        var geocoder;
+        var map;
+        function initMap() {
+            geocoder = new google.maps.Geocoder();
+            var address = document.getElementById('zip_code_geocoding').innerText;
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == 'OK') {
+                    map.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location
+                    });
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+
+            var latlng = new google.maps.LatLng();
+            var mapOptions = {
+                zoom: 14,
+                center: latlng
+            }
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        }
+
+        function codeAddress() {
+            var address = document.getElementById('address').value;
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == 'OK') {
+                    map.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location
+                    });
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        }
+    </script>
+
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDVReW9KtqGqweQhSNFyAqkwMAE25w6RY&callback=initMap">
+    </script>
 <?php
 
 }
