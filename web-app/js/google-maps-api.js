ba1157c7e193553ae6d9663801a1c01c373fc544
Mattias Kallman <33825140+kallmanm@users.noreply.github.com>
2018-05-14 07:49:07 +0300

    small fixes

--- web-app/js/google-maps-api.js
+++ web-app/js/google-maps-api.js
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
 var geocoder;
 var map;
 
-=======
-let geocoder;
-let map;
->>>>>>> 057cefc310cb5eb9e0680a20f6dc60dafbc08d9a
 
 function initMap() {
     geocoder = new google.maps.Geocoder();
-    const address = document.getElementById('zip-code-geocoding').innerText;
+    const address = document.getElementById('zip_code_geocoding').innerText;
     geocoder.geocode( { 'address': address}, function(results, status) {
         if (status === 'OK') {
             map.setCenter(results[0].geometry.location);
-<<<<<<< HEAD
-            /*var marker = new google.maps.Marker({
-=======
-            const marker = new google.maps.Marker({
->>>>>>> 057cefc310cb5eb9e0680a20f6dc60dafbc08d9a
-                map: map,
-                position: results[0].geometry.location
 
-            });*/
 
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
@@ -36,8 +23,8 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
-<<<<<<< HEAD
-=======
+
+
 function codeAddress() {
     const address = document.getElementById('address').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
@@ -52,4 +39,4 @@ function codeAddress() {
         }
     });
 }
->>>>>>> 057cefc310cb5eb9e0680a20f6dc60dafbc08d9a
+
