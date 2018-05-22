776d57b43e20e0b2fef6479f5c7b725bde385d65
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2018-05-23 02:00:22 +0300

    updated database import file

--- web_serv_cms_project_1.sql
+++ web_serv_cms_project_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 17, 2018 at 09:07 AM
+-- Generation Time: May 22, 2018 at 10:59 PM
 -- Server version: 5.6.39
 -- PHP Version: 7.1.14
 
@@ -30,15 +30,17 @@ USE `web_serv_cms_project_1`;
 --
 -- Table structure for table `anomalities_log`
 --
--- Creation: May 05, 2018 at 06:36 PM
+-- Creation: May 20, 2018 at 07:03 AM
 --
 
 DROP TABLE IF EXISTS `anomalities_log`;
-CREATE TABLE `anomalities_log` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `anomalities_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `remote_addr` char(255) NOT NULL,
-  `event` text NOT NULL
+  `event` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -46,17 +48,18 @@ CREATE TABLE `anomalities_log` (
 --
 -- Table structure for table `sessions_log`
 --
--- Creation: May 09, 2018 at 12:15 PM
+-- Creation: May 21, 2018 at 07:31 PM
 --
 
 DROP TABLE IF EXISTS `sessions_log`;
-CREATE TABLE `sessions_log` (
+CREATE TABLE IF NOT EXISTS `sessions_log` (
   `session_id` char(36) NOT NULL,
   `user_id` char(36) NOT NULL,
   `remote_addr` char(255) NOT NULL,
-  `session_start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `session_start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `session_expiration` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `session_end` timestamp NULL DEFAULT NULL
+  `session_end` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -64,12 +67,13 @@ CREATE TABLE `sessions_log` (
 --
 -- Table structure for table `users_main`
 --
--- Creation: May 09, 2018 at 12:15 PM
+-- Creation: May 22, 2018 at 10:35 AM
 --
 
 DROP TABLE IF EXISTS `users_main`;
-CREATE TABLE `users_main` (
+CREATE TABLE IF NOT EXISTS `users_main` (
   `id` char(36) NOT NULL,
+  `gender` tinyint(4) NOT NULL,
   `name` char(255) NOT NULL,
   `name_url_encoded` char(255) NOT NULL,
   `password_hash` char(60) NOT NULL,
@@ -77,48 +81,18 @@ CREATE TABLE `users_main` (
   `zip_code` char(255) NOT NULL,
   `about_you` text NOT NULL,
   `annual_salary` decimal(10,0) NOT NULL,
-  `dating_preference` tinyint(8) NOT NULL
+  `dating_preference` tinyint(8) NOT NULL,
+  `preferences` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `name_url_encoded` (`name_url_encoded`),
+  UNIQUE KEY `email` (`email`),
+  KEY `zip_code` (`zip_code`),
+  KEY `annual_salary` (`annual_salary`),
+  KEY `dating_preference` (`dating_preference`),
+  KEY `name` (`name`),
+  KEY `gender` (`gender`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `anomalities_log`
---
-ALTER TABLE `anomalities_log`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- Indexes for table `sessions_log`
---
-ALTER TABLE `sessions_log`
-  ADD PRIMARY KEY (`session_id`);
-
---
--- Indexes for table `users_main`
---
-ALTER TABLE `users_main`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD UNIQUE KEY `name_url_encoded` (`name_url_encoded`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `zip_code` (`zip_code`),
-  ADD KEY `annual_salary` (`annual_salary`),
-  ADD KEY `dating_preference` (`dating_preference`),
-  ADD KEY `name` (`name`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `anomalities_log`
---
-ALTER TABLE `anomalities_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
